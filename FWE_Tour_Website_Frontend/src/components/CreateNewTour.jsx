f7bb8cac4b507b0884f72790d0afcb02b9bfb882
Dang Minh Nguyen <dangminh214@gmail.com>
2024-05-14 13:04:33 +0200

    display error while tour has the same name, display name of destination after selecting

--- FWE_Tour_Website_Frontend/src/components/CreateNewTour.jsx
+++ FWE_Tour_Website_Frontend/src/components/CreateNewTour.jsx
@@ -5,16 +5,85 @@ const CreateNewTour = () => {
   // State variables to store form data
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
-  const [destinations, setDestinations] = useState('');
-  const [imageCover, setImageCover] = useState('');
+  const [destinations, setDestinations] = useState([]);
+  const [selectedDestination, setSelectedDestination] = useState('');
+  const [imageCover, setImageCover] = useState([]);
+  const [successMessage, setSuccessMessage] = useState('');
+
   const [allDestinations, setAllDestinations] = useState([]);
+  const [showWarning, setShowWarning] = useState(false);
+  const [warningMessage, setWarningMessage] = useState('');
+
+  const selectedArray = [];
+
+  const WarningPopup = ({ message, onClose }) => (
+    <div className="warning-popup">
+      <p>{message}</p>
+      <button onClick={onClose}>Close</button>
+    </div>
+  );
+
+  const closeWarning = () => {
+    setShowWarning(false);
+  };
+
+  const handleImageUrlChange = (index, event) => {
+    const newImageCover = [...imageCover];
+    newImageCover[index] = event.target.value;
+    setImageCover(newImageCover);
+  };
+  
+  // Handler for adding a new image URL input field
+  const handleAddImageUrl = () => {
+    setImageCover([...imageCover, '']);
+  };
+  
+  // Handler for removing an image URL input field
+  const handleRemoveImageUrl = (index) => {
+    setImageCover(imageCover.filter((_, i) => i !== index));
+  };
+
+    // Function to handle adding a destination to the tour
+  const handleAddDestination = () => {
+    if (selectedDestination && !destinations.includes(selectedDestination)) {
+      setDestinations([...destinations, selectedDestination]);
+      selectedArray.push(selectedDestination)
+      setSelectedDestination('');
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const existingTour = await axios.get(`http://localhost:8000/tours/${name}`);
+      if (existingTour.data) {
+        // If a tour with the same name already exists, set the warning message and return
+        setWarningMessage('Dieser Name ist schon existiert, bitte anderen Name benutzen');
+        return;
+      }
+      const newTour = { name, description, destinations, imageCover };
+      if (destinations.length === 0) {
+        setShowWarning(true);
+        return;
+      }
+      const response = await axios.post('http://localhost:8000/tours/newTour', newTour);
+      console.log('New tour created:', response.data);
+      // Set the success message
+      setSuccessMessage('Die Reise wurde erfolgreich erstellt');
+      setName('');
+      setDescription('');
+      setDestinations([]);
+      setImageCover([]);
+    } catch (error) {
+      console.error('Error creating new tour:', error);
+    }
+  };
 
   // Fetch all destinations from backend API
   useEffect(() => {
     const fetchDestinations = async () => {
       try {
         const response = await axios.get('http://localhost:8000/destination');
-        console.log(response)
         setAllDestinations(response.data.destinations);
       } catch (error) {
         console.error('Error fetching destinations:', error);
@@ -24,78 +93,83 @@ const CreateNewTour = () => {
     fetchDestinations();
   }, []);
 
-  // Function to handle form submission
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      // Convert destinations string to an array of strings
-      const destinationsArray = destinations.split(',').map(destination => destination.trim());
-
-      const newTour = { name, description, destinations: destinationsArray, imageCover };
-      // Send POST request to backend API to create a new tour
-      const response = await axios.post('http://localhost:8000/tours/newTour', newTour);
-      console.log('New tour created:', response.data);
-      // Clear form after successful submission
-      setName('');
-      setDescription('');
-      setDestinations('');
-      setImageCover('');
-    } catch (error) {
-      console.error('Error creating new tour:', error);
-    }
-  };
-
   return (
-    <div>
-      <h2>Neue Reise</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="description">Description:</label>
-          <textarea
-            id="description"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            required
-          ></textarea>
-        </div>
-        <div>
-          <label htmlFor="destinations">Destinations:</label>
-          <select
-            id="destinations"
-            value={destinations}
-            onChange={(e) => setDestinations(e.target.value)}
-            required
-          >
-            <option value="">Select a destination...</option>
-            {allDestinations.map(destination => (
-              <option key={destination._id} value={destination._id}>
-                {destination.name}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div>
-          <label htmlFor="imageCover">Image Fotos URL:</label>
-          <input
-            type="text"
-            id="imageCover"
-            value={imageCover}
-            onChange={(e) => setImageCover(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Create Tour</button>
-      </form>
+    <div className="createNewForm">
+        {!successMessage && (
+          <>
+            <h2 className="newFormTitel">Neue Reise</h2>
+            <form onSubmit={handleSubmit}>
+              <div>
+                <label htmlFor="name">Name:</label>
+                <input
+                  type="text"
+                  id="name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  required
+                />
+              </div>
+              <div>
+                <label htmlFor="description">Beschreibung:</label>
+                <textarea
+                  id="description"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                ></textarea>
+              </div>
+              <div>
+                <label htmlFor="destinations">Reiseziele:</label>
+                <select
+                  id="destinations"
+                  value={selectedDestination}
+                  onChange={(e) => setSelectedDestination(e.target.value)}
+                >
+                  <option value="">Select a destination...</option>
+                  {allDestinations.map(destination => (
+                    !destinations.includes(destination._id) && (
+                      <option key={destination.name} value={destination._id}>
+                        {destination.name}
+                      </option>
+                    )
+                  ))}
+                </select>
+                <button type="button" onClick={handleAddDestination}>Reiseziel hinzufügen</button>
+                <ul>
+                {destinations.map((destinationId, index) => {
+                  const destination = allDestinations.find(dest => dest._id === destinationId);
+                  return (
+                    <div key={index}>
+                      <p>{destination ? destination.name : 'Destination not found'}</p>
+                    </div>
+                  );
+                })}
+                </ul>
+              </div>
+
+              <div>
+                <label>Bilder URLs:</label>
+                {imageCover.map((url, index) => (
+                  <div key={index}>
+                    <input
+                      type="text"
+                      value={url}
+                      onChange={(event) => handleImageUrlChange(index, event)}
+                    />
+                    <button type="button" onClick={() => handleRemoveImageUrl(index)}>
+                      Remove
+                    </button>
+                  </div>
+                ))}
+                <button type="button" onClick={handleAddImageUrl}>
+                  Add Image URL
+                </button>
+              </div>
+              {showWarning && <WarningPopup message="Eine Reise muss mindesten ein Ziel haben" onClose={closeWarning} />}
+              <button type="submit">Create Tour</button>
+            </form>
+          </>
+        )}
+      {warningMessage && <div className="warning-popup"><h3>{warningMessage}</h3></div>}
     </div>
   );
 };

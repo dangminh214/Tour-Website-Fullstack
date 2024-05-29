0beda944275f3c0a55fc78551620ad20e5af70ac
dangminh214 <dangminh214@gmail.com>
2024-05-29 18:17:36 +0200

    create a new destination test/delete a destination

--- FWE_Tour_Website_Backend/backend.test.js
+++ FWE_Tour_Website_Backend/backend.test.js
@@ -5,8 +5,6 @@ const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
-const port = process.env.PORT || 8000;
-
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
@@ -38,23 +36,41 @@ describe("GET all destinations: /destination", () => {
   });
 })
 
-describe("GET all destinations: /destination", () => {
-  it("should return all destinations", async () => {
-    const res = await request(app).get("/destination");
-    expect(res.statusCode).toBe(200);
-    expect(res.body.destinations).toBeDefined();
-    expect(res.body.destinations.length).toBeGreaterThan(0);
-    console.log("Number of destinations: ", res.body.destinations.length)
+describe("POST /destination/newDestination", () => {
+  it("should create a new destination", async () => {
+    const newDestination = {
+      name: "Test Destination",
+      description: "This is a test destination",
+      imageCover: [
+        "https://example.com/image1.jpg",
+        "https://example.com/image2.jpg"
+      ]
+    };
+
+    const res = await request(app).post("/destination/newDestination").send(newDestination);
+
+    expect(res.statusCode).toBe(201);
+    expect(res.body.destination).toBeDefined();
+    expect(res.body.destination.name).toBe(newDestination.name);
+    expect(res.body.destination.description).toBe(newDestination.description);
+    expect(res.body.destination.imageCover).toEqual(expect.arrayContaining(newDestination.imageCover));
+    destinationId = res.body.destination._id;
+    destinationName = res.body.destination.name
   });
 })
 
-describe("GET a destinations: /destination/Darmstadt", () => {
+describe("DELETE /destination/:destinationId", () => {
+  it("should delete the test destination", async () => {
+    const res = await request(app).delete(`/destination/deleteADestination/${destinationId}`);
+    expect(res.statusCode).toBe(204);
+  });
+});
+
+describe("GET a destinations: /destination/${destinationName}", () => {
   it("should return a destinationdetail", async () => {
-    const res = await request(app).get("/destination/Darmstadt");
+    const res = await request(app).get(`/destination/${destinationName}`);
     expect(res.statusCode).toBe(200);
-    console.log(res.body)
     expect(res.body).toBeDefined();
-    console.log("Destination detail: ", res.body)
   });
 })
 
@@ -63,7 +79,6 @@ describe("GET a tour: /tours/Let's go Darmstadt", () => {
     const res = await request(app).get("/tours/Let's go Darmstadt");
     expect(res.statusCode).toBe(200);
     expect(res.body.data).toBeDefined();
-    console.log("Tour detail: ", res.body)
   });
 })
 
